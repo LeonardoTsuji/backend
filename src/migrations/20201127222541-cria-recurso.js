3bd37b9d807cb3449155ef67ade06fb6a9901831
LeonardoTsuji <leonardo_tsuji@hotmail.com>
2021-04-04 16:42:05 -0300

    comentando migration

--- src/migrations/20201127222541-cria-recurso.js
+++ src/migrations/20201127222541-cria-recurso.js
@@ -2,36 +2,36 @@
 
 module.exports = {
   up: async (queryInterface, DataTypes) => {
-    return queryInterface.createTable(
-      "resource",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: DataTypes.INTEGER,
-        },
-        name: {
-          allowNull: false,
-          type: DataTypes.STRING,
-          unique: true,
-        },
-        createdAt: {
-          allowNull: false,
-          type: DataTypes.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: DataTypes.DATE,
-        },
-      },
-      {
-        tableName: "resource",
-      }
-    );
+    // return queryInterface.createTable(
+    //   "resource",
+    //   {
+    //     id: {
+    //       allowNull: false,
+    //       autoIncrement: true,
+    //       primaryKey: true,
+    //       type: DataTypes.INTEGER,
+    //     },
+    //     name: {
+    //       allowNull: false,
+    //       type: DataTypes.STRING,
+    //       unique: true,
+    //     },
+    //     createdAt: {
+    //       allowNull: false,
+    //       type: DataTypes.DATE,
+    //     },
+    //     updatedAt: {
+    //       allowNull: false,
+    //       type: DataTypes.DATE,
+    //     },
+    //   },
+    //   {
+    //     tableName: "resource",
+    //   }
+    // );
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("resource");
+    // return queryInterface.dropTable("resource");
   },
 };
--- src/migrations/20201127222542-cria-relacional-regra-recurso.js
+++ src/migrations/20201127222542-cria-relacional-regra-recurso.js
@@ -2,51 +2,51 @@
 
 module.exports = {
   up: async (queryInterface, DataTypes) => {
-    return queryInterface.createTable(
-      "resourceRole",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: DataTypes.INTEGER,
-        },
-        resourceId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: "Resource",
-            key: "id",
-          },
-        },
-        roleId: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          references: {
-            model: "Role",
-            key: "id",
-          },
-        },
-        permission: {
-          allowNull: false,
-          type: DataTypes.STRING,
-        },
-        createdAt: {
-          allowNull: false,
-          type: DataTypes.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: DataTypes.DATE,
-        },
-      },
-      {
-        tableName: "resourceRole",
-      }
-    );
+    // return queryInterface.createTable(
+    //   "resourceRole",
+    //   {
+    //     id: {
+    //       allowNull: false,
+    //       autoIncrement: true,
+    //       primaryKey: true,
+    //       type: DataTypes.INTEGER,
+    //     },
+    //     resourceId: {
+    //       type: DataTypes.INTEGER,
+    //       allowNull: false,
+    //       references: {
+    //         model: "Resource",
+    //         key: "id",
+    //       },
+    //     },
+    //     roleId: {
+    //       type: DataTypes.INTEGER,
+    //       allowNull: false,
+    //       references: {
+    //         model: "Role",
+    //         key: "id",
+    //       },
+    //     },
+    //     permission: {
+    //       allowNull: false,
+    //       type: DataTypes.STRING,
+    //     },
+    //     createdAt: {
+    //       allowNull: false,
+    //       type: DataTypes.DATE,
+    //     },
+    //     updatedAt: {
+    //       allowNull: false,
+    //       type: DataTypes.DATE,
+    //     },
+    //   },
+    //   {
+    //     tableName: "resourceRole",
+    //   }
+    // );
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("resourceRole");
+    // return queryInterface.dropTable("resourceRole");
   },
 };
